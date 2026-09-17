7e8ba353f867ce7b1b4abe0d534fadab9c8ffead
lavialireza <alirezalavi65@gmail.com>
2026-09-17 10:23:02 +0330

    Add files via upload

--- app/src/main/java/com/example/bookapp/ui/screens/AllImagesGalleryScreen.kt
+++ app/src/main/java/com/example/bookapp/ui/screens/AllImagesGalleryScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
